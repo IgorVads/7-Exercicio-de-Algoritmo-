9cc87295547bb9eb73904a9bfa6b588ce9dba426
Igor Vinicius <161034058+IgorVads@users.noreply.github.com>
2024-04-14 08:07:16 -0300

    7° Exercicio de Algoritmo
    Repositório com o objetivo de guardar a minha 7° lista de exercício da aula de Algoritmo em C.

---
+++ EX7/de 1 a 500.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <locale.h>
+
+
+//Um programa que exibe o somatório dos valores pares existentes na faixa
+//de 1 (inclusive) até 500 (inclusive).
+
+
+int main(int argc, char** argv)
+{
+	int i, total = 0;
+	
+	setlocale(LC_ALL, "portuguese");
+	
+	printf("Somatório de 1 até 500: ");
+	
+	for (i = 1; i <= 500; i++) {
+				if (i % 2 == 0) 
+				{
+					total = i + total;
+				}
+					
+	}		
+			printf("\n\nO resultado final do somatório é de: %i\n", total);
+	return 0;
+}
